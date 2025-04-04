586cc8f6d09dd1d1e4e4e1fdafb0be4560a45b4e
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-04 20:54:13 +0200

    Added Sudoku implementation
    - Added a new case when the number is duplicated in a grid.
    - Implement the new case when the number is duplicated in a grid.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.math.sqrt
 
 fun main() {
 
@@ -5,6 +6,13 @@ fun main() {
 
 fun isValidSudoku(sudokuBoard: Array<Array<String>>): Boolean {
     if (sudokuBoard.isEmpty()) return false
+
+    if (!sudokuBoard.size.isPerfectSquare()
+        && sudokuBoard.size % 2 != 0
+        && sudokuBoard.size != 3
+    ) {
+        return false
+    }
 
     val allowedRange = 1..sudokuBoard.size
 
@@ -18,7 +26,7 @@ fun isValidSudoku(sudokuBoard: Array<Array<String>>): Boolean {
             val isValidSudokuCell = cell.isValidSudokuCell(allowedRange)
             if (!isValidSudokuCell) return false
 
-            if (hasRepeatedNumbers(row)) {
+            if (row.toList().hasRepeatedNumbers()) {
                 //Check if row has duplicate cells
                 return false
             }
@@ -31,8 +39,16 @@ fun isValidSudoku(sudokuBoard: Array<Array<String>>): Boolean {
     }
 
     columns.forEach { column ->
-        if (hasRepeatedNumbers(column)) {
+        if (column.toList().hasRepeatedNumbers()) {
             //Check if column has duplicate cells
+            return false
+        }
+    }
+
+    val grids = extractSubGrids(sudokuBoard)
+    grids.forEach { grid ->
+        if (grid.hasRepeatedNumbers()) {
+            //Check if grid has duplicate cells
             return false
         }
     }
@@ -40,10 +56,57 @@ fun isValidSudoku(sudokuBoard: Array<Array<String>>): Boolean {
     return true
 }
 
-fun hasRepeatedNumbers(row: Array<String>) : Boolean{
+fun extractSubGrids(grid: Array<Array<String>>): List<List<String>> {
+    val gridSize = grid.size
+    val (subGridRow, subGridColumn) = getSubGridRowColumn(gridSize)
+
+    val subGrids = mutableListOf<List<String>>()
+
+    for (rowStart in 0..<gridSize step subGridRow) {
+        for (colStart in 0..<gridSize step subGridColumn) {
+            val subgrid = mutableListOf<String>()
+
+            for (row in rowStart..<rowStart + subGridRow) {
+                for (col in colStart..<colStart + subGridColumn) {
+                    subgrid.add(grid[row][col])
+                }
+            }
+            subGrids.add(subgrid)
+        }
+    }
+    return subGrids
+}
+
+fun getSubGridRowColumn(gridSize: Int): Pair<Int, Int> {
+    var subGridRow = 0
+    var subGridColumn = 0
+    if (gridSize.isPerfectSquare()) {
+        val sqrtValue = sqrt(gridSize.toDouble()).toInt()
+        subGridRow = sqrtValue
+        subGridColumn = sqrtValue
+    } else {
+        //10x10, 6x6
+        for (i in 2..gridSize) {
+            if (gridSize % i == 0) {
+                subGridRow = i
+                subGridColumn = gridSize / subGridRow
+                break
+            }
+        }
+    }
+    return Pair(subGridRow, subGridColumn)
+}
+
+fun Int.isPerfectSquare(): Boolean {
+    if (this < 0) return false // Negative numbers can't be perfect squares
+    val sqrtValue = sqrt(this.toDouble()).toInt()
+    return sqrtValue * sqrtValue == this
+}
+
+fun <T> Collection<T>.hasRepeatedNumbers(): Boolean {
     val emptyCell = "-"
-    val rowWithoutEmptyChars = row.filter { it !=  emptyCell}
-    return rowWithoutEmptyChars.distinct() .size != rowWithoutEmptyChars.size
+    val rowWithoutEmptyChars = this.filter { it != emptyCell }
+    return rowWithoutEmptyChars.distinct().size != rowWithoutEmptyChars.size
 }
 
 fun String.isValidSudokuCell(allowedRange: IntRange): Boolean {
